a7b4e4bc04e40ec781102130a8de5c2531b48747
Alex Grintsvayg <git@grin.io>
2018-03-23 19:18:00 -0400

    add transaction manager, fix bencoding to support int keys, fix routing table bucketing

---
+++ cmd/dht.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"github.com/lbryio/reflector.go/dht"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	var cmd = &cobra.Command{
+		Use:   "dht",
+		Short: "Run interactive dht node",
+		Run:   dhtCmd,
+	}
+	RootCmd.AddCommand(cmd)
+}
+
+func dhtCmd(cmd *cobra.Command, args []string) {
+	dht, err := dht.New(&dht.Config{
+		Address:    "127.0.0.1:21216",
+		SeedNodes:  []string{"127.0.0.1:21215"},
+		PrintState: true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dht.Run()
+}
