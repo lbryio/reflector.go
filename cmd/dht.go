9979a70c613af2a9ca70c9cabc210274c880eeb7
Alex Grintsvayg <git@grin.io>
2018-04-24 18:12:17 -0400

    fix rt updates, add Ping method

--- cmd/dht.go
+++ cmd/dht.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"math/rand"
 	"os"
 	"os/signal"
@@ -11,6 +13,7 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/lbryio/reflector.go/dht"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -24,6 +27,44 @@ func init() {
 }
 
 func dhtCmd(cmd *cobra.Command, args []string) {
+	type rtData struct {
+		BlobHashes []string `json:"blob_hashes"`
+		Buckets    map[string][]struct {
+			Address string   `json:"address"`
+			Blobs   []string `json:"blobs"`
+			NodeID  string   `json:"node_id"`
+		} `json:"buckets"`
+		Contacts []string `json:"contacts"`
+		NodeID   string   `json:"node_id"`
+	}
+
+	bytes, err := ioutil.ReadAll(os.Stdin)
+	checkErr(err)
+
+	var data rtData
+	err = json.Unmarshal(bytes, &data)
+	checkErr(err)
+
+	spew.Dump(data)
+
+	d, err := dht.New(nil)
+	checkErr(err)
+	err = d.Start()
+	checkErr(err)
+
+	for _, nodes := range data.Buckets {
+		for _, node := range nodes {
+			err = d.Ping(node.Address + ":4444")
+			if err != nil {
+				log.Errorf("no response from %s", node.Address)
+			}
+		}
+	}
+
+	d.Shutdown()
+
+	return
+
 	rand.Seed(time.Now().UnixNano())
 
 	//d, err := dht.New(&dht.Config{
