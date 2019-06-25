fb2f807e6aa0f5137333cbf594ab7b72d4194e77
Alex Grintsvayg <git@grin.io>
2019-06-25 13:08:35 -0400

    temporarily disable reflector and db

--- cmd/reflector.go
+++ cmd/reflector.go
@@ -3,14 +3,12 @@ package cmd
 import (
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
 	"time"
 
 	"github.com/lbryio/reflector.go/db"
 	"github.com/lbryio/reflector.go/meta"
 	"github.com/lbryio/reflector.go/peer"
-	"github.com/lbryio/reflector.go/reflector"
 	"github.com/lbryio/reflector.go/store"
 
 	log "github.com/sirupsen/logrus"
@@ -35,22 +33,22 @@ func reflectorCmd(cmd *cobra.Command, args []string) {
 	}
 
 	s3 := store.NewS3BlobStore(globalConfig.AwsID, globalConfig.AwsSecret, globalConfig.BucketRegion, globalConfig.BucketName)
-	combo := store.NewDBBackedS3Store(s3, db)
+	//combo := store.NewDBBackedS3Store(s3, db)
 
-	reflectorServer := reflector.NewServer(combo)
-	reflectorServer.Timeout = 3 * time.Minute
-	if globalConfig.SlackHookURL != "" {
-		reflectorServer.StatLogger = log.StandardLogger()
-		reflectorServer.StatReportFrequency = 1 * time.Hour
-	}
-	reflectorServer.EnableBlocklist = true
+	//reflectorServer := reflector.NewServer(combo)
+	//reflectorServer.Timeout = 3 * time.Minute
+	//if globalConfig.SlackHookURL != "" {
+	//	reflectorServer.StatLogger = log.StandardLogger()
+	//	reflectorServer.StatReportFrequency = 1 * time.Hour
+	//}
+	//reflectorServer.EnableBlocklist = true
+	//
+	//err = reflectorServer.Start(":" + strconv.Itoa(reflector.DefaultPort))
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
-	err = reflectorServer.Start(":" + strconv.Itoa(reflector.DefaultPort))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	peerServer := peer.NewServer(combo)
+	peerServer := peer.NewServer(s3)
 	if globalConfig.SlackHookURL != "" {
 		peerServer.StatLogger = log.StandardLogger()
 		peerServer.StatReportFrequency = 1 * time.Hour
@@ -64,5 +62,5 @@ func reflectorCmd(cmd *cobra.Command, args []string) {
 	signal.Notify(interruptChan, os.Interrupt, syscall.SIGTERM)
 	<-interruptChan
 	peerServer.Shutdown()
-	reflectorServer.Shutdown()
+	//reflectorServer.Shutdown()
 }
