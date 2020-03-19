e2766b1a70c9266517d627911e3843b67b1494d4
Niko Storni <niko@lbry.io>
2020-03-19 05:30:21 +0100

    increase packet size
    add retro-compatibility
    remove unused SPV servers
    remove travis test failure

--- cmd/reflector.go
+++ cmd/reflector.go
@@ -7,10 +7,12 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/lbryio/reflector.go/peer"
+	"github.com/lbryio/reflector.go/peer/quic"
+
 	"github.com/lbryio/reflector.go/db"
 	"github.com/lbryio/reflector.go/internal/metrics"
 	"github.com/lbryio/reflector.go/meta"
-	"github.com/lbryio/reflector.go/peer"
 	"github.com/lbryio/reflector.go/reflector"
 	"github.com/lbryio/reflector.go/store"
 
@@ -19,6 +21,10 @@ import (
 )
 
 var reflectorCmdCacheDir string
+var peerPort int
+var quicPeerPort int
+var reflectorPort int
+var metricsPort int
 
 func init() {
 	var cmd = &cobra.Command{
@@ -27,6 +33,10 @@ func init() {
 		Run:   reflectorCmd,
 	}
 	cmd.Flags().StringVar(&reflectorCmdCacheDir, "cache", "", "Enable disk cache for blobs. Store them in this directory")
+	cmd.Flags().IntVar(&peerPort, "peer-port", 5567, "The port reflector will distribute content from")
+	cmd.Flags().IntVar(&quicPeerPort, "quic-peer-port", 5568, "The port reflector will distribute content from over QUIC protocol")
+	cmd.Flags().IntVar(&reflectorPort, "reflector-port", 5566, "The port reflector will receive content from")
+	cmd.Flags().IntVar(&metricsPort, "metrics-port", 2112, "The port reflector will use for metrics")
 	rootCmd.AddCommand(cmd)
 }
 
@@ -57,7 +67,7 @@ func reflectorCmd(cmd *cobra.Command, args []string) {
 		reflectorServer.Timeout = 3 * time.Minute
 		reflectorServer.EnableBlocklist = true
 
-		err = reflectorServer.Start(":" + strconv.Itoa(reflector.DefaultPort))
+		err = reflectorServer.Start(":" + strconv.Itoa(reflectorPort))
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -72,12 +82,18 @@ func reflectorCmd(cmd *cobra.Command, args []string) {
 	}
 
 	peerServer := peer.NewServer(blobStore)
-	err = peerServer.Start(":5567")
+	err = peerServer.Start(":"+ strconv.Itoa(peerPort))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	metricsServer := metrics.NewServer(":2112", "/metrics")
+	quicPeerServer := quic.NewServer(blobStore)
+	err = quicPeerServer.Start(":"+ strconv.Itoa(quicPeerPort))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	metricsServer := metrics.NewServer(":" + strconv.Itoa(metricsPort), "/metrics")
 	metricsServer.Start()
 
 	interruptChan := make(chan os.Signal, 1)
@@ -85,6 +101,7 @@ func reflectorCmd(cmd *cobra.Command, args []string) {
 	<-interruptChan
 	metricsServer.Shutdown()
 	peerServer.Shutdown()
+	quicPeerServer.Shutdown()
 	if reflectorServer != nil {
 		reflectorServer.Shutdown()
 	}
